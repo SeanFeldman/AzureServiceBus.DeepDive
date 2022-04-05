871e51cae132554bba46a1f79bc37469e5fc8437
Sean Feldman <feldman.sean@gmail.com>
2022-04-05 16:02:18 -0600

    Clean up plugins code

---
+++ Plugins/Plugins.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Azure.Core.Amqp;
+using Azure.Messaging.ServiceBus;
+
+namespace Plugins;
+
+public static class Plugins
+{
+    public static Func<ServiceBusMessage, Task> PrefixPlugin => message =>
+    {
+        message.Body = new BinaryData($"---PREFIX---{Environment.NewLine}{message.Body}");
+
+        return Task.CompletedTask;
+    };
+
+    public static Func<ServiceBusReceivedMessage, Task> PostfixPlugin => message =>
+    {
+        var amqpMessage = message.GetRawAmqpMessage();
+
+        amqpMessage.Body.TryGetData(out var data);
+        amqpMessage.Body.TryGetValue(out var value);
+
+        // WARNING - more sophisticated than this
+        // See https://github.com/Azure/azure-sdk-for-net/issues/12943 for discussion
+        var original = Encoding.UTF8.GetString(data.First().Span);
+        var modified = $"{original}{Environment.NewLine}---SUFFIX---";
+        var bytes = Encoding.UTF8.GetBytes(modified);
+
+        amqpMessage.Body = new AmqpMessageBody(new ReadOnlyMemory<byte>[] { new(bytes) });
+
+        return Task.CompletedTask;
+    };
+}
--- Plugins/Program.cs
+++ Plugins/Program.cs
@@ -1,9 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
 using System.Threading.Tasks;
-using Azure.Core.Amqp;
 using Azure.Messaging.ServiceBus;
 
 namespace Plugins;
@@ -37,31 +34,4 @@ internal class Program
 
         await serviceBusClient.DisposeAsync();
     }
-}
-
-public class Plugins
-{
-    public static Func<ServiceBusMessage, Task> PrefixPlugin => message =>
-    {
-        message.Body = new BinaryData($"---PREFIX---{Environment.NewLine}{message.Body}");
-
-        return Task.CompletedTask;
-    };
-
-    public static Func<ServiceBusReceivedMessage, Task> PostfixPlugin => message =>
-    {
-        var amqpMessage = message.GetRawAmqpMessage();
-
-        amqpMessage.Body.TryGetData(out var data);
-
-        // WARNING - more sophisticated than this
-        // See https://github.com/Azure/azure-sdk-for-net/issues/12943 for discussion
-        var original = Encoding.UTF8.GetString(data.First().Span);
-        var modified = $"{original}{Environment.NewLine}---SUFFIX---";
-        var bytes = Encoding.UTF8.GetBytes(modified);
-
-        amqpMessage.Body = new AmqpMessageBody(new ReadOnlyMemory<byte>[] { new(bytes) });
-
-        return Task.CompletedTask;
-    };
 }
